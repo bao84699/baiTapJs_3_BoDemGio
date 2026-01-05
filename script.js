bb03ea275b47ea32c805aa27a0c094097e0cbe53
bao84699 <bao84699@gmail.com>
2026-01-05 21:48:19 +0700

    v02

--- script.js
+++ script.js
@@ -2,35 +2,46 @@ let start = document.getElementById("start");
 let stop = document.getElementById("stop");
 let reset = document.getElementById("reset");
 let display = document.getElementById("display");
+
 let h = 0;
 let m = 0;
 let s = 0;
+let ms = 0;
 let kt = 0;
 
-start.addEventListener("click", ()=> {
-    if (kt == 0) {
-        kt = setInterval(()=> {
-            s +=1;
-            if (s == 60){
-                m+=1;
+start.addEventListener("click", () => {
+    if (kt === 0) {
+        kt = setInterval(() => {
+            ms += 10;
+
+            if (ms === 1000) {
+                s++;
+                ms = 0;
+            }
+
+            if (s === 60) {
+                m++;
                 s = 0;
             }
-            if (m == 60){
-                h += 1;
+
+            if (m === 60) {
+                h++;
                 m = 0;
             }
-            display.textContent = String(h).padStart(2,"0") + ":" + String(m).padStart(2,"0") + ":" + String(s).padStart(2,"0");
-        },1000)
+
+            display.textContent = String(h).padStart(2, "0") + ":" + String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0") + ":" + String(ms/10).padStart(2, "0");
+        }, 10);
     }
-})
+});
 
-stop.addEventListener("click", ()=>{
+stop.addEventListener("click", () => {
     clearInterval(kt);
     kt = 0;
-})
+});
 
-reset.addEventListener("click", ()=> {
+reset.addEventListener("click", () => {
     clearInterval(kt);
     kt = 0;
-    display.textContent = "00:00:00";
-})
+    h = m = s = ms = 0;
+    display.textContent = "00:00:00:00";
+});
